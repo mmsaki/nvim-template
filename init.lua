af95e0ebde411471054e233df9564b450e6127bd
Meek Msaki <meek10x@gmail.com>
2025-07-31 06:33:53 -0500

    remove lsps from init and restor plugin files

--- init.lua
+++ init.lua
@@ -42,26 +42,4 @@ require("lazy").setup({
   },
 })
 
--- lsp
--- configurations for all tables found in lsp/<name>.lua
-vim.lsp.config("*", {
-  capabilities = {
-    textDocument = {
-      semanticTokens = {
-        multilineTokenSupport = true,
-      },
-    },
-  },
-  root_markers = { ".git" },
-})
-
-vim.lsp.enable("luals")
-vim.lsp.enable("markdown")
-vim.lsp.enable("solc-ls")
-vim.lsp.enable("pyright")
-vim.lsp.enable("ruff")
--- vim.lsp.enable("json")
--- vim.lsp.enable("solc")
--- vim.lsp.enable("solidity_ls_nomicfoundation")
-
 -- vim: ts=2 sts=2 sw=2 et
--- plugin/options.lua
+++ plugin/options.lua
@@ -1,6 +1,6 @@
 vim.g.have_nerd_font = true
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
 local opt = vim.opt
@@ -9,7 +9,9 @@ opt.number = true
 opt.tabstop = 2
 opt.softtabstop = 2
 opt.shiftwidth = 2
-opt.clipboard = 'unnamedplus'
-opt.signcolumn = 'yes:1'
+opt.clipboard = "unnamedplus"
+opt.signcolumn = "yes:1"
 opt.scrolloff = 10
 opt.cursorline = true
+
+vim.o.winborder = "rounded"
