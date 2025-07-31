588ec85f04d418c41724d2f479006ad59678d811
Meek Msaki <meek10x@gmail.com>
2025-07-31 06:36:34 -0500

    add diagnostic and completion autocmds

---
+++ plugin/diagnostic.lua
@@ -0,0 +1,7 @@
+vim.diagnostic.config({
+  virtual_lines = true,
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
