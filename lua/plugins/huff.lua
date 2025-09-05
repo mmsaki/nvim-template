b3d1a6623a3b4a3c46dea6b5c4606269ed9aaacf
Meek Msaki <meek10x@gmail.com>
2025-09-05 08:11:04 -0500

    add huff

--- lua/plugins/huff.lua
+++ lua/plugins/huff.lua
@@ -1,5 +1,8 @@
 return {
-  "mmsaki/huff.nvim",
+  -- "wuwe1/vim-huff",
+  -- ft = "huff",
+  dir = "/Users/meek/Developer/huff.nvim",
+  -- "mmsaki/huff.nvim",
   version = "0.2.*",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
