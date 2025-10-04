9a1a962e11fd2a65f6984f3d37cd3059d8b8066a
Meek Msaki <meek10x@gmail.com>
2025-10-04 16:00:47 -0500

    use deployed plugin

--- lua/plugins/huff.lua
+++ lua/plugins/huff.lua
@@ -1,8 +1,8 @@
 return {
   -- "wuwe1/vim-huff",
   -- ft = "huff",
-  dir = "/Users/meek/Developer/huff.nvim",
-  -- "mmsaki/huff.nvim",
+  -- dir = "/Users/meek/Developer/huff.nvim",
+  "mmsaki/huff.nvim",
   version = "0.2.*",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
