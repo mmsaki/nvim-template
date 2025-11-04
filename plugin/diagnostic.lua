0f904f689b1db6fb275936ea61e1cd56043663e3
Meek Msaki <meek10x@gmail.com>
2025-11-03 19:31:13 -0600

    add dignostics per line

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -1,7 +1,7 @@
 vim.diagnostic.config({
-  virtual_lines = true,
-  -- virtual_lines = {
+  -- virtual_lines = true,
+  virtual_lines = {
   --  -- Only show virtual line diagnostics for the current cursor line
-  --  current_line = true,
-  -- },
+   current_line = true,
+  },
 })
