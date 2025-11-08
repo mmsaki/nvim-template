b27fc0d681c28d277ecaaba12db880829362580f
Meek Msaki <meek10x@gmail.com>
2025-11-07 21:11:50 -0600

    add diagnostic, remove colortheme

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,5 +4,16 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  opts = {
+    diagnostics = {
+      enable = true,
+      show_on_dirs = true,
+      show_on_open_dirs = true,
+      debounce_delay = 500,
+      severity = {
+        min = vim.diagnostic.severity.HINT,
+        max = vim.diagnostic.severity.ERROR,
+      },
+    },
+  },
 }
