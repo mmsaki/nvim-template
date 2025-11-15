df484e24d09b141128b53059aa5d11225087a039
Meek Msaki <meek10x@gmail.com>
2025-11-15 01:27:12 -0600

    add markdown plugin

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    heading = {
+      enabled = false,
+      render_modes = false,
+    },
+    code = {
+      enabled = false,
+    },
+  },
+}
