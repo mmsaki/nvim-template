75a13f5198a95efd8dd72b23fc88787b6d95837e
Meek Msaki <meek10x@gmail.com>
2025-11-15 01:30:41 -0600

    add bione root

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -23,4 +23,23 @@ return {
     "package.json",
     ".git",
   },
+  root_dir = function(fname)
+    local root_files = { "biome.json", "biome.jsonc" }
+    root_files = util.insert_package_json(root_files, "biome", fname)
+    return vim.fs.dirname(
+      vim.fs.find(root_files, { path = fname, upward = true })[1]
+    )
+  end,
+  single_file_support = true,
+  docs = {
+    description = [[
+https://biomejs.dev
+
+Toolchain of the web. [Successor of Rome](https://biomejs.dev/blog/annoucing-biome).
+
+```sh
+npm install [-g] @biomejs/biome
+```
+]],
+  },
 }
