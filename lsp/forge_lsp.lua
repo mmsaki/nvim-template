e2a27859fe2749f1b4fe0f8e43b8c919a7534759
Meek Msaki <meek10x@gmail.com>
2025-12-08 18:55:15 -0600

    update forge lsp

--- lsp/forge_lsp.lua
+++ lsp/forge_lsp.lua
@@ -1,12 +1,10 @@
 return {
   name = "Forge Lsp",
-  -- Run `foundryup -i nightly`
-  -- Nigthly version to access forge lint
-  cmd = { "/Users/meek/developer/foundry/target/release/forge", "lsp" },
+  -- Install binary from https://crates.io/crates/forge-lsp
+  -- cargo install forge-lsp
+  cmd = { "forge-lsp" },
+  -- cmd = { "/Users/meek/developer/lsp/target/release/forge-lsp" },
   root_dir = vim.fs.root(0, { "foundry.toml", ".git" }),
   filetypes = { "solidity" },
   root_markers = { "foundry.toml", ".git" },
 }
--- TODO: This is a
--- very long
--- comment (just imagine it)
