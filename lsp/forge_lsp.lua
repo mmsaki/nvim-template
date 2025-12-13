9df12d3e1a54f2d3950f74c6d744e50e0f421254
Meek Msaki <meek10x@gmail.com>
2025-12-12 22:06:40 -0600

    add status line

--- lsp/forge_lsp.lua
+++ lsp/forge_lsp.lua
@@ -2,8 +2,8 @@ return {
   name = "Forge Lsp",
   -- Install binary from https://crates.io/crates/forge-lsp
   -- cargo install forge-lsp
-  cmd = { "forge-lsp" },
-  -- cmd = { "/Users/meek/developer/lsp/target/release/forge-lsp" },
+  -- cmd = { "forge-lsp" },
+  cmd = { "/Users/meek/developer/lsp/target/release/forge-lsp" },
   root_dir = vim.fs.root(0, { "foundry.toml", ".git" }),
   filetypes = { "solidity" },
   root_markers = { "foundry.toml", ".git" },
