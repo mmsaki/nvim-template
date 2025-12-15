b054f77db760b43ae836ac5eb60770098b0af78f
Meek Msaki <meek10x@gmail.com>
2025-12-15 02:32:46 -0600

    moved lsp dir

--- lsp/forge_lsp.lua
+++ lsp/forge_lsp.lua
@@ -3,7 +3,7 @@ return {
   -- Install binary from https://crates.io/crates/forge-lsp
   -- cargo install forge-lsp
   -- cmd = { "forge-lsp" },
-  cmd = { "/Users/meek/developer/lsp/target/release/forge-lsp" },
+  cmd = { "/Users/meek/developer/mmsaki/lsp/target/release/forge-lsp" },
   root_dir = vim.fs.root(0, { "foundry.toml", ".git" }),
   filetypes = { "solidity" },
   root_markers = { "foundry.toml", ".git" },
