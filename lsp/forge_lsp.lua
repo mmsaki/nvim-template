12354b19917fc026fb25a062aa539e5914bc6853
Meek Msaki <meek10x@gmail.com>
2025-12-25 19:28:38 -0600

    support for lsp formatting

--- lsp/forge_lsp.lua
+++ lsp/forge_lsp.lua
@@ -3,8 +3,21 @@ return {
   -- Install binary from https://crates.io/crates/forge-lsp
   -- cargo install forge-lsp
   -- cmd = { "forge-lsp" },
-  cmd = { "/Users/meek/developer/mmsaki/lsp/target/release/forge-lsp" },
+  -- cmd = { "/Users/meek/developer/mmsaki/lsp/target/release/forge-lsp" },
+  -- cmd = { "solidity-ls", "--stdio" },
+  cmd = {
+    "/Users/meek/developer/mmsaki/solidity-language-server/target/release/solidity-language-server",
+  },
   root_dir = vim.fs.root(0, { "foundry.toml", ".git" }),
   filetypes = { "solidity" },
   root_markers = { "foundry.toml", ".git" },
+  on_attach = function(_, _)
+    -- NOTE: BufWritePost allows client to save first, then run lsp formatting
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = { "*.sol" },
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end,
 }
