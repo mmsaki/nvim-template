838842e49e1d70f1c69df7e7139a6caf57e97a3e
Meek Msaki <meek10x@gmail.com>
2026-02-04 20:35:57 -0600

    format with python is faster

--- lsp/json.lua
+++ lsp/json.lua
@@ -4,7 +4,7 @@ return {
   cmd = { "vscode-json-language-server", "--stdio" },
   filetypes = { "json", "jsonc" },
   init_options = {
-    provideFormatter = true,
+    provideFormatter = false,
   },
   settings = {
     json = {
@@ -13,12 +13,26 @@ return {
       },
     },
   },
-  on_attach = function(_, _)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = { "*.json" },
+  on_attach = function(_, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = { "*.json", "*.jsonc" },
       callback = function()
-        vim.lsp.buf.format()
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+        local content = table.concat(lines, "\n") .. "\n"
+        local python_code = [[import json, sys
+content = sys.stdin.read()
+try:
+    data = json.loads(content)
+    print(json.dumps(data, indent=2))
+except:
+    print(content)
+]]
+        local result = vim.fn.system({ "python3", "-c", python_code }, content)
+        if vim.v.shell_error == 0 then
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(result, "\n"))
+        end
       end,
     })
   end,
 }
+
