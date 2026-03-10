97cbc0d8682bdcd64b2fd8bb039103bc303b61de
Meek Msaki <meek10x@gmail.com>
2026-03-10 05:33:52 -0500

    add call hierachy quicklist

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -43,6 +43,52 @@ vim.lsp.enable("zig")
 -- vim.lsp.enable("solidity_ls_nomicfoundation")
 -- vim.lsp.enable("solc")
 
+-- Custom call hierarchy handlers: show target definition location
+-- instead of the call-site fromRanges that the default quickfix uses.
+vim.lsp.handlers["callHierarchy/incomingCalls"] = function(_, result, ctx)
+  if not result or vim.tbl_isempty(result) then
+    vim.notify("No incoming calls found", vim.log.levels.INFO)
+    return
+  end
+  local items = {}
+  for _, call in ipairs(result) do
+    local item = call.from
+    local uri = item.uri
+    local range = item.selectionRange
+    local filename = vim.uri_to_fname(uri)
+    table.insert(items, {
+      filename = filename,
+      lnum = range.start.line + 1,
+      col = range.start.character + 1,
+      text = item.name .. (item.detail and (" " .. item.detail) or ""),
+    })
+  end
+  vim.fn.setqflist({}, " ", { title = "Incoming Calls", items = items })
+  vim.cmd("copen")
+end
+
+vim.lsp.handlers["callHierarchy/outgoingCalls"] = function(_, result, ctx)
+  if not result or vim.tbl_isempty(result) then
+    vim.notify("No outgoing calls found", vim.log.levels.INFO)
+    return
+  end
+  local items = {}
+  for _, call in ipairs(result) do
+    local item = call.to
+    local uri = item.uri
+    local range = item.selectionRange
+    local filename = vim.uri_to_fname(uri)
+    table.insert(items, {
+      filename = filename,
+      lnum = range.start.line + 1,
+      col = range.start.character + 1,
+      text = item.name .. (item.detail and (" " .. item.detail) or ""),
+    })
+  end
+  vim.fn.setqflist({}, " ", { title = "Outgoing Calls", items = items })
+  vim.cmd("copen")
+end
+
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
   callback = function(event)
