3c44f65e7a6fbed1a452ba3575a6d56dfe822194
Meek Msaki <meek10x@gmail.com>
2026-05-02 09:21:22 -0500

    remove loop

--- lua/plugins/oil-git.lua
+++ lua/plugins/oil-git.lua
@@ -10,12 +10,5 @@ return {
       },
     })
     require("oil-git-status").setup()
-    for _, hl_group in pairs(require("oil-git-status").highlight_groups) do
-      if hl_group.index then
-        vim.api.nvim_set_hl(0, hl_group.hl_group, { fg = "#00ff00" })
-      else
-        -- vim.api.nvim_set_hl(0, hl_group.hl_group, { fg = "#ff0000" })
-      end
-    end
   end,
 }
